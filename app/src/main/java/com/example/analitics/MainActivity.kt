e928b276ed5c1adbce4aceba14ee5fa93c4a870f
Igor <igorek.chernov007@gmail.com>
2020-10-30 15:47:01 +0200

    modify a bit design

--- app/src/main/java/com/example/analitics/MainActivity.kt
+++ app/src/main/java/com/example/analitics/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.analitics
 
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,13 +16,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val button = findViewById<Button>(R.id.press_button)
+        
 
         press_f.setOnClickListener {
             enter.text = "count: ${++counter}"
 
         }
+        bodo4.setOnClickListener {
+            Toast.makeText(this, "Hi there", Toast.LENGTH_LONG).show()
+        }
+
     }
+
 }
